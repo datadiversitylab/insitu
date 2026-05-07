6baf6ed1bf50d14857d1bce44d5c7ff6fcdcfbe1
kmartinet <kmartinet@outlook.com>
2026-05-07 11:40:48 -0700

    Good version of match_island_phylo, draft of classify_tips

--- R/classify_tips.R
+++ R/classify_tips.R
@@ -1,7 +1,60 @@
-#' Assigns tips as island endemic, non-endemic, or widespread based on locality data
+#' Assigns tips as island endemic or non-endemic based on locality data
+#'
+#' This function classifies species both on the island-level and on
+#' the archipelago-level. For example, a species that occurs in two islands within
+#' the same archipelago is not endemic on an island-level (0 in the island column),
+#' but is endemic on an archipelago-level (1 in the archipelago column).
+#'
+#' @param occs Occurrence data (in a data.frame) that includes information about
+#' several species and where they occur. This function assumes that there is a column
+#' called "species" where the species name is written, and a column called "locale"
+#' where the corresponding locale is written. If one species occupies multiple locales,
+#' there should be multiple rows for that species in this dataframe.
+#' @param islands A vector of island names that help classify each tip.
+#'
 #'
 #' @export
 
-classify_tips <- function(){
-  return(1+1)
+classify_tips <- function(occs, islands){
+  # Check the occs dataframe and determine:
+  # 1. How many locales each species occupies
+  # 2. Whether each species is island endemic or non-endemic at each level
+  #    (island vs archipelago)
+
+  # How many locales does each species occupy?
+  occs_locality <- occs |>
+    # Group by species to account for one species having multiple rows
+    dplyr::group_by(.data$species) |>
+    # Add all localities for each species, with a comma separating them
+    dplyr::summarize(locality = paste(.data$locale, collapse = ", "))
+
+  # Create a new dataframe that will be populated with info about endemicity
+  endemic <- as.data.frame(matrix(nrow = length(occs_locality$species),
+                                  ncol = 3))
+  # Add the species column as the first column
+  endemic[,1] <- occs_locality$species
+  # Change column names
+  colnames(endemic) <- c("species", "archipelago", "island")
+
+  # For each species, determine whether it's endemic at each level
+  #  (island and archipelago)
+  for(i in c(1:length(occs_locality$locality))){
+    # Split the string by commas, and remove extra spacing
+    locale_vec <- stringr::str_split(occs_locality$locality[i], ",\\s*")[[1]]
+
+    # Check if all of the islands are within the archipelago
+    if(all(locale_vec %in% islands)){
+      endemic$archipelago[i] <- 1
+    } else {
+      endemic$archipelago[i] <- 0
+    }
+
+    # If locale_vec is more than one entry, then the species is not endemic at
+    #  the island level
+    if(length(locale_vec) > 0){
+      endemic$island[i] <- 0
+    } else {
+      endemic$island[i] <- 1
+    }
+  } # End for
 }
---
+++ R/match_island_phylo.R
@@ -0,0 +1,91 @@
+#' Imports and validates dated phylogenies with associated island occurrence data
+#'
+#'
+#' @param phy The phylogenetic tree associated with your data
+#' @param locs A dataframe with 2 columns: "species" and "locale". Each row
+#' should represent a single locality where that species is located (e.x., if
+#' a species is found in multiple localities, there should be multiple rows for
+#' that species). This package focuses on island occurrences, so if a species is
+#' found on the mainland, write its locality as "Mainland".
+#' @param exclude (Logical) Set to TRUE if you want to exclude mainland species
+#' from the output. Default: TRUE
+#' @return A list with 3 items:
+#' * `PAM`: A presence-absence matrix describing where species occur
+#' * `sp_df`: A dataframe summarizing what species, and how many, are on each island
+#' * `phy`: A pruned version of the user-provided phylogeny that includes only species that are in the user-provided locality data
+#'
+#' @export
+
+match_island_phylo <- function(phy, locs, exclude = TRUE){
+  # If the user wants to exclude mainland species, do so
+  if(exclude == TRUE){
+    locs <- locs[which(locs$locale != "Mainland"),]
+  }
+
+  # Make sure that each species in locs is also present in the tree
+  # If not, remove the species that don't match (and make a note about it)
+
+  # Create a dataframe with just species names for ease of using treedata
+  temp_df <- as.data.frame(unique(locs$species))
+  rownames(temp_df) <- temp_df[,1]
+
+  td <- geiger::treedata(phy, temp_df, warnings = FALSE)
+
+  # Write a nice message about which species were dropped in each case
+  # Difference between data and phy
+  # (the tips that were dropped from phy because they were not in data)
+  drop_sp <- phy$tip.label[!(phy$tip.label %in% td$phy$tip.label)]
+  # Write the message
+  if (!getOption("insitu.silent", FALSE)) {
+    cli::cli_alert_info("Species dropped from the tree because they were not in the data: {drop_sp}")
+  }
+
+  # Difference between phy and data
+  # (species that were dropped from data because they were not in phy)
+  # Dataframe from treedata (with dropped species)
+  temp_dat <- as.data.frame(td$data)
+  colnames(temp_dat) <- "species"
+
+  drop_sp <- temp_df[,1][!(temp_df[,1] %in% temp_dat$species)]
+  # Write the message
+  if (!getOption("insitu.silent", FALSE)) {
+    cli::cli_alert_info("Species dropped from the data because they were not in the tree: {drop_sp}")
+  }
+
+  # Save the pruned tree
+  phy <- td$phy
+
+  # Remove species that were not in the tree from the locality data
+  final_locs <- dplyr::inner_join(x = locs, y = temp_dat, by = "species")
+
+  # Next, create the PAM and sp_df objects to return
+  # Create PAM
+  PAM <- reshape2::dcast(
+    final_locs,
+    locale ~ species,
+    fun.aggregate = length,
+    value.var = "species"
+  )
+
+  # Create sp_df, with a list of all species on each island and a count
+  sp_df <- final_locs |>
+    # First, group by island
+    dplyr::group_by(.data$locale) |>
+    # Next, paste all of the species into one list and count them
+    dplyr::summarize(
+      species_list = paste(.data$species, collapse = ", "),
+      richness = dplyr::n_distinct(.data$species)
+    )
+
+  results <- list(
+    PAM = PAM,
+    sp_df = sp_df,
+    phy = phy
+  )
+
+  # Print out head of sp_df to allow the user to quickly check it
+  if (!getOption("insitu.silent", FALSE)) {
+    cli::cli_alert_info("Matching complete! Here are the first 5 locales in your data:")
+    print(utils::head(sp_df, n = 5))
+  }
+}
