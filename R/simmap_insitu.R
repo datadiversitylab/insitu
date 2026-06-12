4057eaf0d55808e2192e93f219ef411bb3dfdeb5
cromanpa94 <cromanpa94@gmail.com>
2026-06-12 12:27:27 -0500

    Support for multiphylo

--- R/simmap_insitu.R
+++ R/simmap_insitu.R
@@ -1,5 +1,9 @@
 #' Uses stochastic character mapping to propagate ASR uncertainty through speciation classifications. Posterior distribution of in-situ event counts
 #'
+#' When multiple trees are provided, the function propagates ASR uncertainty
+#' from the stochastic mapping itself, and phylogenetic uncertainty from
+#' the distribution of trees.
+#'
 #' @param phy The phylogenetic tree associated with your data
 #' @param PAM A presence-absence matrix reflecting where each species of
 #' interest is located. The `match_island_phylo` function will create this PAM,
@@ -17,28 +21,48 @@
 #' @export
 
 simmap_insitu <- function(phy, PAM, model = "ER", nsim = 10){
+  # Accept either a single tree or a list of trees
+  if (inherits(phy, "phylo")) phy <- list(phy)
+
   # First, run an ancestral state reconstruction per island separately
   # Get the list of islands
   islands <- PAM$locale
 
-  # Create an empty list to save reconstructions
-  sim_recons <- list()
+  # Run stochastic maps for each tree
+  island_accumulator <- vector("list", length(islands))
+  names(island_accumulator) <- islands
 
-  # For each island in the PAM, run make.simmap and save the output for
-  #  each internal node
-  for(i in 1:length(islands)){
-    # The full row except the first column (where locales are)
-    # Needs to be a vector for ace
-    island_states <- unlist(PAM[i, -1])
-    # Save the per-node reconstruction in the list under the name of the island
-    sim_recons[[islands[i]]] <- summary(phytools::make.simmap(tree = phy,
-                                                              x = island_states,
-                                                              model = model,
-                                                              nsim = nsim,
-                                                              Q = "mcmc",
-                                                              message = FALSE))$ace
+  for (tree in phy) {
+    for (i in seq_along(islands)) {
+      island_states <- unlist(PAM[i, -1])
+
+      # When trees differ in tips
+      island_states <- island_states[names(island_states) %in% tree$tip.label]
+
+      ace_mat <- summary(
+        phytools::make.simmap(
+          tree    = tree,
+          x       = island_states,
+          model   = model,
+          nsim    = nsim,
+          Q       = "mcmc",
+          message = FALSE
+        )
+      )$ace
+
+      if (is.null(island_accumulator[[islands[i]]])) {
+        island_accumulator[[islands[i]]] <- ace_mat
+      } else {
+        # Average over shared nodes
+        shared <- intersect(
+          rownames(island_accumulator[[islands[i]]]),
+          rownames(ace_mat)
+        )
+        island_accumulator[[islands[i]]][shared, ] <-
+          (island_accumulator[[islands[i]]][shared, ] + ace_mat[shared, ]) / 2
+      }
+    }
   }
 
-  # Return the list
-  return(sim_recons)
+  return(island_accumulator)
 }
